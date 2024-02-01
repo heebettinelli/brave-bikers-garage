1bbcc1a15dba87b10d793efe766d52ee25858f53
Helen <fer.helen@icloud.com>
2024-02-01 11:54:15 +0000

    build: changes language on html and comment on js code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function googleTranslateElementInit(){
     new google.tranlate.TranslateElement ({pageLanguage: 'en'}, 'google_translate_element');
 }
+
+// make button to translate and link with this function
