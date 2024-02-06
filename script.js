f7107f69a88d9d153bb90376fe6fbc31f08e968e
Helen <fer.helen@icloud.com>
2024-02-06 15:00:09 +0000

    build: creates translate pages. style: changes font-weight and add h1

--- script.js
+++
@@ -1,5 +0,0 @@
-function googleTranslateElementInit(){
-    new google.tranlate.TranslateElement ({pageLanguage: 'en'}, 'google_translate_element');
-}
-
-// make button to translate and link with this function
